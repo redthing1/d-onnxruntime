732a81d878d2eea718f7582e651c508a6415a998
redthing1 <redthing1@alt.icu>
2024-07-15 01:25:58 -0700

    stub to run ort inference

--- demo/source/app.d
+++ demo/source/app.d
@@ -20,6 +20,35 @@ void ort_abort_on_error(const(OrtApi)* g_ort, OrtStatus* status) {
     }
 }
 
+// template <typename T>
+// static void softmax(T& input) {
+//   float rowmax = *std::max_element(input.begin(), input.end());
+//   std::vector<float> y(input.size());
+//   float sum = 0.0f;
+//   for (size_t i = 0; i != input.size(); ++i) {
+//     sum += y[i] = std::exp(input[i] - rowmax);
+//   }
+//   for (size_t i = 0; i != input.size(); ++i) {
+//     input[i] = y[i] / sum;
+//   }
+// }
+void softmax(T)(ref T input) {
+    float rowmax = float.min;
+    for (size_t i = 0; i < input.length; i++) {
+        if (input[i] > rowmax) {
+            rowmax = input[i];
+        }
+    }
+    float[] y = new float[input.length];
+    float sum = 0.0f;
+    for (size_t i = 0; i < input.length; i++) {
+        sum += y[i] = exp(input[i] - rowmax);
+    }
+    for (size_t i = 0; i < input.length; i++) {
+        input[i] = y[i] / sum;
+    }
+}
+
 int main(string[] args) {
     writefln("loading onnxruntime v%s", ORT_API_VERSION);
     const(OrtApi)* g_ort = OrtGetApiBase().GetApi(ORT_API_VERSION);
@@ -62,6 +91,83 @@ int main(string[] args) {
     status = g_ort.CreateSession(env, mnist_model_path.ptr, session_options, &session);
     ort_abort_on_error(g_ort, status);
     writefln("session created");
+
+    // attempt to infer
+    //      OrtMemoryInfo* memory_info;
+    //   ORT_ABORT_ON_ERROR(g_ort->CreateCpuMemoryInfo(OrtArenaAllocator, OrtMemTypeDefault, &memory_info));
+    //   const int64_t input_shape[] = {1, 3, 720, 720};
+    //   const size_t input_shape_len = sizeof(input_shape) / sizeof(input_shape[0]);
+    //   const size_t model_input_len = model_input_ele_count * sizeof(float);
+
+    //   OrtValue* input_tensor = NULL;
+    //   ORT_ABORT_ON_ERROR(g_ort->CreateTensorWithDataAsOrtValue(memory_info, model_input, model_input_len, input_shape,
+    //                                                            input_shape_len, ONNX_TENSOR_ELEMENT_DATA_TYPE_FLOAT,
+    //                                                            &input_tensor));
+    //   assert(input_tensor != NULL);
+    //   int is_tensor;
+    //   ORT_ABORT_ON_ERROR(g_ort->IsTensor(input_tensor, &is_tensor));
+    //   assert(is_tensor);
+    //   g_ort->ReleaseMemoryInfo(memory_info);
+    //   const char* input_names[] = {"inputImage"};
+    //   const char* output_names[] = {"outputImage"};
+    //   OrtValue* output_tensor = NULL;
+    //   ORT_ABORT_ON_ERROR(g_ort->Run(session, NULL, input_names, (const OrtValue* const*)&input_tensor, 1, output_names, 1,
+    //                                 &output_tensor));
+    //   assert(output_tensor != NULL);
+    //   ORT_ABORT_ON_ERROR(g_ort->IsTensor(output_tensor, &is_tensor));
+    //   assert(is_tensor);
+    //   int ret = 0;
+    //   float* output_tensor_data = NULL;
+    //   ORT_ABORT_ON_ERROR(g_ort->GetTensorMutableData(output_tensor, (void**)&output_tensor_data));
+    //   uint8_t* output_image_data = NULL;
+    //   chw_to_hwc(output_tensor_data, 720, 720, &output_image_data);
+    //   if (write_image_file(output_image_data, 720, 720, output_file) != 0) {
+    //     ret = -1;
+    //   }
+    //   g_ort->ReleaseValue(output_tensor);
+    //   g_ort->ReleaseValue(input_tensor);
+    //   free(model_input);
+
+    OrtMemoryInfo* memory_info;
+    writefln("creating cpu memory info");
+    status = g_ort.CreateCpuMemoryInfo(OrtArenaAllocator, OrtMemTypeDefault, &memory_info);
+    ort_abort_on_error(g_ort, status);
+
+    // mnist input shape is 1,1,28,28
+    const long[] input_shape = [1, 1, 28, 28];
+    const size_t model_input_len = 1 * 1 * 28 * 28 * float.sizeof;
+    float* model_input = cast(float*) malloc(
+        input_shape[0] * input_shape[1] * input_shape[2] * input_shape[3] * float.sizeof);
+
+    OrtValue* input_tensor;
+    writefln("creating input tensor");
+    status = g_ort.CreateTensorWithDataAsOrtValue(memory_info, model_input, model_input_len, input_shape.ptr, input_shape
+            .length, ONNX_TENSOR_ELEMENT_DATA_TYPE_FLOAT, &input_tensor);
+    ort_abort_on_error(g_ort, status);
+    assert(input_tensor !is null, "failed to create input tensor");
+    int is_tensor;
+    status = g_ort.IsTensor(input_tensor, &is_tensor);
+    ort_abort_on_error(g_ort, status);
+
+    g_ort.ReleaseMemoryInfo(memory_info);
+
+    char*[] input_names = [cast(char*) "Input3"];
+    char*[] output_names = [cast(char*) "Plus214_Output_0"];
+
+    OrtValue* output_tensor;
+
+    writefln("running inference");
+    status = g_ort.Run(session, null, input_names.ptr, &input_tensor, 1, output_names.ptr, 1, &output_tensor);
+    ort_abort_on_error(g_ort, status);
+    assert(output_tensor !is null, "failed to run inference");
+    g_ort.IsTensor(output_tensor, &is_tensor);
+    assert(is_tensor, "output is not a tensor");
+
+    int ret = 0;
+    float* output_tensor_data;
+    status = g_ort.GetTensorMutableData(output_tensor, cast(void**)&output_tensor_data);
+    ort_abort_on_error(g_ort, status);
+    assert(output_tensor_data !is null, "failed to get output tensor data");
 
     // clean up
     g_ort.ReleaseSessionOptions(session_options);
